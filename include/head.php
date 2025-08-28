75acee12e17d5da74ba18241431cdab092ab3945
Dio-Damar-Danendra-Portofolio <diodamar14102000@gmail.com>
2025-08-28 17:54:23 +0700

    Reupdate

--- include/head.php
+++ include/head.php
@@ -10,8 +10,8 @@
   <script src="https://cdn.jsdelivr.net/npm/read-excel-file@5.5.5/umd/read-excel-file.min.js"></script>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js"></script>
-  <link href="../frontend/styling.css" rel="stylesheet">
-  <script src="../frontend/external_link.js"></script>
+  <link href="frontend/styling.css" rel="stylesheet">
+  <script src="frontend/external_link.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
 </head>
